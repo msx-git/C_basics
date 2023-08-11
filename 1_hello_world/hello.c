90be2de25236ac6303ffe6bd0047f2c46706474e
Musoxon <msx.tuit@gmail.com>
2023-08-11 23:34:47 +0500

    first commit

---
+++ 1_hello_world/hello.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, world!");
+    return 0;
+}
+
+
+// Comments
+
+/* A single-line C-style comment. */
+
+/* A multi-line
+   C-style comment. */
+
+/*
+ * A very common way to 
+ * format a multi-line
+ * C-Style comment.
+ */
+
+/* C-style comments can appear almost anywhere. */
+
+// A more common way 
+// of commenting with multi-line
+// C++ style comments
+
+// Anything can appear after //, even /* ... */ and
+// even more // after the first // but they will be 
+// ignored because they are all in the comment.
+/* eof */
